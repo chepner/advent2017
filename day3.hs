96f0699455dd5def009e53cb2f91dcdcf2223fbd
Clint Hepner <clint.hepner@gmail.com>
2017-12-07 04:10:40 -0500

    Grid computation complete
    Just need to figure out the first one that writes a value
    greater than the input.

--- day3.hs
+++ day3.hs
@@ -1,3 +1,5 @@
+import qualified Data.Map.Strict as M
+
 -- Puzzle 1: manhattan distance from input to 1
 -- on the spiral grid
 --
@@ -25,9 +27,11 @@ segment i = replicate i GoRight ++ replicate i GoUp ++ replicate (i+1) GoLeft ++
 grid :: [Dir]
 grid = [1,3..] >>= segment
 
--- Trial-and-error shows that 289444 is the upper-left corner 
--- puzzle1 n' = let n = read n' in take 320 $ drop (n - 160) (zip [1..] grid)
-puzzle1 n = let (x, y) = scanl update (0,0) grid !! ((read n) - 1)
+
+computePositions :: [(Int, Int)]
+computePositions = scanl update (0,0) grid
+
+puzzle1 n = let (x, y) = computePositions !! ((read n) - 1)
             in abs x + abs y
  
 
@@ -38,8 +42,30 @@ update (x, y) = \d -> case d of
                          GoUp -> (x, y + 1)
                          GoDown -> (x, y - 1)
 
+-- Puzzle 2 - fill in order each square with the sum of adjacent squares
+--            unvisited squares are zeroed
+-- s1 == 1
+-- s2 == s1
+-- s3 == s1 + s2
+-- s4 == s1 + s2 + s3
+-- s5 == s1 + s4
+type Grid = M.Map (Int, Int) Integer
+
+
+neighbors (x,y) = (,) <$> [x-1,x,x+1] <*> [y-1,y,y+1]
+
+addPosition :: (Int, Int) -> Grid -> Grid
+addPosition square g = M.insert square value g
+                       where value = sum [M.findWithDefault 0 p g | p <- neighbors square]
+
+foo = scanl (flip addPosition) (M.singleton (0,0) 1) (drop 1 computePositions)
+
+
+
+
+--stressTest = scanl addPosition (M.singleton (0,0) 1) computePositions
 puzzle2 = id
 
 main = do 
-     puzzle1 <$> readFile "day3.input"  >>= print
-     puzzle2 <$> readFile "day3.input"  >>= print
+     puzzle1 <$> readFile "day3.input"  >>= print  -- 419
+     puzzle2 <$> readFile "day3.input"  >>= print  -- ???
