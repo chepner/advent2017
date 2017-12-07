a4e833e2c3433cf5a5766ee4bb857cc61ca297aa
Clint Hepner <clint.hepner@gmail.com>
2017-12-07 09:14:14 -0500

    Solved Day 4, puzzle 2

--- day4.hs
+++ day4.hs
@@ -1,14 +1,19 @@
+import Data.List
 import qualified Data.Set as S
 import Control.Arrow
 
 isValidPassphrase :: [String] -> Bool
 isValidPassphrase pp = length pp == length (S.fromList pp)
 
+isValidPassphrase2 :: [String] -> Bool
+isValidPassphrase2 pp = length pp == length (S.fromList (map sort pp))
+
 puzzle1 :: String -> Int
 puzzle1 = length . filter (isValidPassphrase . words) . lines
                
-puzzle2 = const 0
+puzzle2 :: String -> Int
+puzzle2 = length . filter (isValidPassphrase2 . words) . lines
 
 main = do 
      puzzle1 <$> readFile "day4.input"  >>= print -- 451
-     puzzle2 <$> readFile "day4.input"  >>= print
+     puzzle2 <$> readFile "day4.input"  >>= print -- 223
